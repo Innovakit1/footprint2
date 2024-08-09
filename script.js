735a122b9d8fab7624f0b9b60ef1fda7cca1d65f
Angie Catherin Oviedo <116136336+Innovakit1@users.noreply.github.com>
2024-08-09 15:02:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let personas = [];
 let preguntaActual = 0;
 const preguntas = [
+    "¿Cuántas personas viven en tu casa?",
+    "¿Consumo promedio de gas en la factura (m3)?",
+    "¿Consumo promedio de agua en la factura (m3)?",
+    "¿Consumo promedio de luz en la factura (kWh)?",
     "¿Cuántas horas de vuelos NACIONALES realizaste en el último año?",
     "¿Cuántas horas de vuelos INTERNACIONALES realizaste en el último año?",
     "¿Cuál es tu principal medio de transporte al trabajo?",
@@ -69,7 +73,11 @@ function mostrarSiguientePregunta() {
             pregunta === "¿Cuál es la distancia en kilómetros de tu casa al trabajo?"
         ) {
             form.innerHTML += `<input type="text" id="respuesta" required>`;
-        } 
+        } else if (pregunta === "¿Cuántas personas viven en tu casa?") {
+            form.innerHTML += `<input type="number" id="respuesta" min="1" required>`;
+        } else {
+            form.innerHTML += `<input type="number" id="respuesta" required>`;
+        }
     } else {
             document.getElementById('preguntas').style.display = 'none';
             calcularHuella();
@@ -115,21 +123,31 @@ function guardarDatos() {
 
 function calcularHuella() {
     personas.forEach(persona => {
-        const vuelos_nacionales = persona[preguntas[0]];
+        const personasHogar = persona[preguntas[0]];
+        const consumo_gas_personal = persona[preguntas[1]] / personasHogar;
+        const CO2_gasnatural = consumo_gas_personal * 2.75 / 1000; // t CO2 / mes
+
+        const consumo_agua_personal = persona[preguntas[2]] / personasHogar;
+        const CO2_agua = consumo_agua_personal * 4 / 1000; // t CO2 / mes
+
+        const consumo_luz_personal = persona[preguntas[3]] / personasHogar;
+        const CO2_luz = consumo_luz_personal * 0.16438 / 1000; // t CO2 / mes
+
+        const vuelos_nacionales = persona[preguntas[4]];
         const CO2_vuelos_nacionales = vuelos_nacionales * 100 / 1000 / 12; // t CO2 / mes
 
-        const vuelos_internacionales = persona[preguntas[1]];
+        const vuelos_internacionales = persona[preguntas[5]];
         const CO2_vuelos_internacionales = vuelos_internacionales * 100 / 1000 / 12; // t CO2 / mes
 
-        const transporte_trabajo = persona[preguntas[2]];
-        const distancia_trabajo = persona[preguntas[3]];
+        const transporte_trabajo = persona[preguntas[6]];
+        const distancia_trabajo = persona[preguntas[7]];
         const CO2_transporte_trabajo = transporte_trabajo * distancia_trabajo * 5 * 4 / 1000; // t CO2 / mes
 
         // Calcular CO2 de la alimentación según el tipo de dieta habitual
-        const dieta = parseFloat(persona[preguntas[4]]);
+        const dieta = parseFloat(persona[preguntas[8]]);
         const CO2_alimentacion = dieta * 30 / 1000; // t CO2 / mes
 
-        const CO2_total = CO2_vuelos_nacionales + CO2_vuelos_internacionales + CO2_transporte_trabajo + CO2_alimentacion;
+        const CO2_total = CO2_gasnatural + CO2_agua + CO2_luz + CO2_vuelos_nacionales + CO2_vuelos_internacionales + CO2_transporte_trabajo + CO2_alimentacion;
 
         persona.CO2_total = CO2_total;
     });
@@ -142,7 +160,7 @@ function mostrarResultados() {
     let resultadosHTML = '';
     personas.forEach(persona => {
         const CO2_anual = persona.CO2_total * 12;
-        resultadosHTML += `<p class="resultado-texto">${persona.nombre}, tu huella de carbono al mes es de ${persona.CO2_total.toFixed(2)} toneladas de carbono. Es decir, que en el último año tu huella de carbono fue de ${CO2_anual.toFixed(2)} toneladas de CO2.</p>`;
+        resultadosHTML += `<p class="resultado-texto">${persona.nombre}, tu huella de carbono al mes es de ${persona.CO2_total.toFixed(2)} toneladas de carbono. Es decir que, en el último año tu huella de carbono fue de ${CO2_anual.toFixed(2)} toneladas de CO2.</p>`;
 
         if (CO2_anual > 7) {
             resultadosHTML += `
@@ -194,7 +212,7 @@ function mostrarCuantosArboles() {
 
     const resultadosHTML = `
         <h2 style="font-size: 20px; color: #000000;">¡¡Puedes empezar sembrando árboles!!</h2>
-        <p style="font-size: 20px; color: #000000; margin: 0;">Según tus resultados, para compensar tu huella de carbono necesitas sembrar aproximadamente</p>
+        <p style="font-size: 18px; color: #000000; margin: 0;">Según tus resultados, para compensar tu huella de carbono, necesitas sembrar aproximadamente</p>
         <p style="font-size: 23px; color: #4CAF50; margin: 0;">${sumaArboles} árboles al año.</p>
         <h2 style="font-size: 20px; color: #000000;">O puedes reducir el consumo de carne, caminar más y practicar el reciclaje.</h2>
         <img src="arboles.png" alt="arboles" width="600" height="206" style="float: left; margin-right: 10px;">
